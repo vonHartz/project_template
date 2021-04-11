533357694493f9d9c314717c42d90a0fc788e792
Danny <stolld@cs.uni-freiburg.de>
2021-04-11 05:08:41 +0200

    add hydra plugin slurm submission

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,3 +7,7 @@ if remove_submit_folder:
 remove_report_folder = '{{cookiecutter.create_report_dir}}' != 'Yes: Create report directory'
 if remove_report_folder:
     shutil.rmtree("reports")
+
+remove_hydra_submit = '{{cookiecutter.submit_strategy}}' != 'Use hydra plugin'
+if remove_hydra_submit:
+    shutil.rmtree("{{cookiecutter.package_name}}/configs/run_slurm.yaml")
